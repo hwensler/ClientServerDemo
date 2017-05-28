31fa4fc2e09d6ba07abb8b5b0475510e364891e9
hw <heatherwensler@gmail.com>
2017-05-28 12:57:58 -0700

    added cmake to clion

---
+++ cmake-build-debug/CMakeFiles/ClientServerGuessingGame.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClientServerGuessingGame.dir/Client.cpp.obj"
+  "CMakeFiles/ClientServerGuessingGame.dir/Server.cpp.obj"
+  "ClientServerGuessingGame.pdb"
+  "ClientServerGuessingGame.exe"
+  "ClientServerGuessingGame.exe.manifest"
+  "libClientServerGuessingGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClientServerGuessingGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ClientServerGuessingGame.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ClientServerGuessingGame.
+# This may be replaced when dependencies are built.
