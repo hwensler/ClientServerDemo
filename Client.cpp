199b048fbe9b6263971a1ecec5af1c567865ff19
hw <heatherwensler@gmail.com>
2017-05-28 11:42:50 -0700

    added appropriate client header files

--- Client.cpp
+++ Client.cpp
@@ -2,3 +2,9 @@
  * The client side of a client-server guessing game.
  * Author: Heather Wensler
  */
+
+#include <netinet/in.h>
+#include <resolv.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
