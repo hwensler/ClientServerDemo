b662d71858935d122fc31aecb3e35e7c3fa2831b
hw <heatherwensler@gmail.com>
2017-05-28 11:49:14 -0700

    Created the socket

--- Server.cpp
+++ Server.cpp
@@ -16,3 +16,15 @@
 
 //a function to handle the sockets
 void* SocketHandler(void*);
+
+int main(int argv, char** argc) {
+
+    //declare the port
+    int host_port = 12400;
+
+    //initialize a socket
+    int thisSocket;
+
+    //create the socket
+    thisSocket = socket(AF_INET, SOCK_STREAM), 0);
+}
