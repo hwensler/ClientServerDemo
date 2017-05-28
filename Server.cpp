fc5ea1f296d4d6b603de2457344f750e4a536caf
hw <heatherwensler@gmail.com>
2017-05-28 11:42:28 -0700

    added appropriate server header files

--- Server.cpp
+++ Server.cpp
@@ -2,3 +2,14 @@
  * The server side of a client-server number guessing game.
  * Author: Heather Wensler
  */
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <resolv.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
